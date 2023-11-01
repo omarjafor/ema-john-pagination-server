0348fc5d1c597f8265f097fccbba2564e0d8a8f0
omarjafor <omarjaforchy@gmail.com>
2023-11-01 11:51:24 +0600

    url parameter hits in the server

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async (req, res) => {
+      console.log('Pagination', req.query);
       const result = await productCollection.find().toArray();
       res.send(result);
     })
